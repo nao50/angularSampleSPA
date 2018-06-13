e0f8fc08e0ccc8f6fd719938c2381d6d48b7d29c
naoyamaguchi <naoyamaguchi0128@gmail.com>
2018-06-14 07:10:47 +0900

    add login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatFormFieldModule,
+} from '@angular/material';
+
+// component
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +25,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    ReactiveFormsModule,
+    // material
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginformgroup = new FormGroup({
+    'email': new FormControl('', [
+      Validators.required
+    ]),
+    'password': new FormControl('', [
+      Validators.required
+    ]),
+  });
+
+
+  // loginformgroup = this.builder.group({
+  //   email: new FormControl('' , [
+  //     Validators.required,
+  //     Validators.minLength(5),
+  //     Validators.maxLength(5),
+  //   ]),
+  //   password: new FormControl('' , [
+  //     Validators.required,
+  //     Validators.minLength(8),
+  //     Validators.maxLength(72),
+  //   ]),
+  // });
+
+  constructor(
+    // private builder: FormBuilder,
+  ) { }
 
   ngOnInit() {
   }
 
+  login() {
+    console.log(this.loginformgroup.value.email);
+    // console.log(this.loginformgroup.controls.email);
+  }
 }
